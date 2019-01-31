066aa326db32a2fa267ecb7a3ea6e4c946a73161
Matthew Shaw <mshaw@sparta.com>
2019-01-31 10:23:52 +0000

    working code

--- js/index.js
+++ js/index.js
@@ -1,66 +1,69 @@
 console.log("linked");
-var game = {
-  players : [
-    {
-      name: "Mat",
-      wins: 0
-    },
-    {
-      name: "Com",
-      wins: 0
-    }
-  ],
-  moves: [
-    {rock: 1},
-    {paper: 2},
-    {scissors: 3}
-  ]
-}
+
 
 console.log(game.players[1].wins);
 //
 
-var turn = parseInt(prompt("Enter move"));
 
-var comTurn = Math.floor(Math.random() * 3) + 1;
-console.log(turn);
-console.log(comTurn);
+// console.log(turn);
+// console.log(comTurn);
+var wins = 0;
+var losses = 0;
+do{
+  var turn = prompt("Enter move");
+
+  var comTurn = Math.floor(Math.random() * 3) + 1;
 switch(turn){
-  case 1:
+  case '1':
   switch(comTurn){
     case 1:
     alert("Draw!")
     break;
     case 2:
     alert("Lose!")
+    losses++;
     break;
     case 3:
     alert("Win!")
+    wins++;
     break;
   }
+  break;
   case '2':
   switch(comTurn){
-    case '1':
+    case 1:
     alert("Win!")
+    wins++;
     break;
-    case '2':
+    case 2:
     alert("Draw!")
     break;
-    case '3':
+    case 3:
     alert("Lose!")
+    losses++;
     break;
   }
+  break;
   case '3':
   switch(comTurn){
-    case '1':
+    case 1:
     alert("Lose!")
+    losses++;
     break;
-    case '2':
+    case 2:
     alert("Win!")
+    wins++;
     break;
-    case '3':
+    case 3:
     alert("Draw!")
     break;
+  }
+  break;
+}
+// turn = "";
+// comTurn = 0;
+console.log(wins);
+console.log(losses);
+}
 
-}
-}
+while( (wins + losses) < 5);
